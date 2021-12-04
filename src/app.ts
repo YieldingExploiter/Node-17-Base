1ee3aa2e413e8fad08259b63b5c094c7ac206f4e
Yielding Coder <90570076+YieldingExploiter@users.noreply.github.com>
2021-12-04 17:50:48 +0000

    things

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,21 @@
+// Log Stuff
 console.log("Running using NodeJS " + process.version)
 
-// Express App
+// Import Express
 import * as express from 'express';
 const app = express();
 
+// Welcome Route
 app.all('/',(req,res)=>{
-  res.send(`<link rel="stylesheet" href="https://discoursewebhook.yieldingcoder.repl.co/.css"><ctr>Running Node@${process.version.replace('v','')}<br/><a href="https://github.com/YieldingExploiter/Node-17-Base" target="_blank">About this</a></ctr>`)
+  res.send(`<link rel="stylesheet" href="https://discoursewebhook.yieldingcoder.repl.co/.css"><ctr><h3>Welcome</h3><hr>Running Node@${process.version.replace('v','')}<br/><a href="https://github.com/YieldingExploiter/Node-17-Base" target="_blank">About this</a></ctr>`)
 })
 
+// 404
 app.use((req,res)=>{
   res.status(404).send(`<link rel="stylesheet" href="https://discoursewebhook.yieldingcoder.repl.co/.css"><ctr><h3>404</h3><hr>This page was not found!</ctr>`)
 })
 
+// Listen
 const port = 42069;
 app.listen(port, () => {
   console.log('Listening on port %s', port)
