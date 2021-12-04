b5af725de06a84a5fb085aeeb8e64e5f474dc20a
Yielding Coder <90570076+YieldingExploiter@users.noreply.github.com>
2021-12-04 17:46:34 +0000

    Mostly readme changes

--- src/app.ts
+++ src/app.ts
@@ -5,10 +5,14 @@ import * as express from 'express';
 const app = express();
 
 app.all('/',(req,res)=>{
-  res.send(`<link rel="stylesheet" href="https://discoursewebhook.yieldingcoder.repl.co/.css"><ctr>Running Node@${process.version.replace('v','')}<br/></ctr>`)
+  res.send(`<link rel="stylesheet" href="https://discoursewebhook.yieldingcoder.repl.co/.css"><ctr>Running Node@${process.version.replace('v','')}<br/><a href="https://github.com/YieldingExploiter/Node-17-Base" target="_blank">About this</a></ctr>`)
+})
+
+app.use((req,res)=>{
+  res.status(404).send(`<link rel="stylesheet" href="https://discoursewebhook.yieldingcoder.repl.co/.css"><ctr><h3>404</h3><hr>This page was not found!</ctr>`)
 })
 
 const port = 42069;
 app.listen(port, () => {
-  console.log('Running on port %s', port)
+  console.log('Listening on port %s', port)
 });
